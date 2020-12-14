0234dc0f440fe16ff5e26a6d3080edcdb95db02a
jeanbapt-sanchez <sanchez.jeanbapt@gmail.com>
2020-12-14 21:45:51 +0100

    adding react cra, tailwind, stylus, setting up and clean organisation & code

---
+++ arthouse-app/src/components/App.js
@@ -0,0 +1,13 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span>Hello World</span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
